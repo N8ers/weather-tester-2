129f5f32c34b408dce8b88993bcf4f6696c51fd5
N8ers <nathanraysheryak@gmail.com>
2018-09-06 12:48:51 -0400

    added sass

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 $(document).ready(function() {
     $('#weatherLocation').click(function() {
-      const city = $('#location').val();
+      let city = $('#location').val();
       $('#location').val("");
-  
-      let request = new XMLHttpRequest();
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a106239c326e4b268a05bd6c1d2f755d`;
-  
-      request.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-          const response = JSON.parse(this.responseText);
-          getElements(response);
+      $.ajax({
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=API-KEY-GOES-HERE`,
+        type: 'GET',
+        data: {
+          format: 'json'
+        },
+        success: function(response) {
+          $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+          $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+        },
+        error: function() {
+          $('#errors').text("There was an error processing your request. Please try again.");
         }
-      }
-  
-      request.open("GET", url, true);
-      request.send();
-  
-     const getElements = function(response) {
-        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-        console.log('bottom of last function');
-      }
+      });
     });
   });
